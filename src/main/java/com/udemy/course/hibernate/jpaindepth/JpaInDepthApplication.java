87a88f45b8aeee6c927cb6743d1413b5dd1af721
Essam Eldin <esam.eldin.ahmed@gmail.com>
2018-09-29 14:57:01 +0200

    all relations+unit testing

--- src/main/java/com/udemy/course/hibernate/jpaindepth/JpaInDepthApplication.java
+++ src/main/java/com/udemy/course/hibernate/jpaindepth/JpaInDepthApplication.java
@@ -1,8 +1,5 @@
 package com.udemy.course.hibernate.jpaindepth;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.udemy.course.hibernate.jpaindepth.entity.Course;
-import com.udemy.course.hibernate.jpaindepth.entity.Student;
 import com.udemy.course.hibernate.jpaindepth.repository.CourseRepository;
 import com.udemy.course.hibernate.jpaindepth.repository.StudentRepository;
 
@@ -38,13 +33,13 @@ public class JpaInDepthApplication implements CommandLineRunner {
 //        log.info("course for id 1001 {}",courseRepository.findById(10001L));
 //        courseRepository.playWithEntityManager();
         //studentRepository.playWithEntityManager();
-        List<Course> courses=new ArrayList<>();
-        courses.add(new Course("course_11"));
-        courses.add(new Course("course_22"));
+//        List<Course> courses=new ArrayList<>();
+//        courses.add(new Course("course_11"));
+//        courses.add(new Course("course_22"));
       //  log.info("student for id 2001L {}",studentRepository.findById(2001L).getCourses());
       // courseRepository.addReviewsToCourse();
       //  log.info("course for id 10001L {}",courseRepository.findById(10001L));
       
-        studentRepository.insertStudentAndCourses(new Student("amged"), courses);
+     //   studentRepository.insertStudentAndCourses(new Student("amged"), courses);
     }
 }
